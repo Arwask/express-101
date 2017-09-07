d0f384fcd09d89611ab0354a192453ff5cff7a51
Arwa <arwa.it15@gmail.com>
2017-09-07 15:38:02 -0500

    notes for express-101

--- app.js
+++ app.js
@@ -4,30 +4,39 @@ let app = express();
 
 //middleware ---> things that happen between request and response
 
-const logParams = (req, res, next) => {
-    console.log("Middleware function");
-    console.log('req.url', req.url);
-    // console.log('req', req);
-    next(); // <---- move to next middleware. done with this one. It goes to whatver comes next in app.use() but if the nexxt one does not match with the specified url, it will skip and move to the next one
-};
+// const logParams = (req, res, next) => {
+//     console.log("Middleware function");
+//     console.log('req.url', req.url);
+//     // console.log('req', req);
+//     next(); // <---- move to next middleware. done with this one. It goes to whatver comes next in app.use() but if the nexxt one does not match with the specified url, it will skip and move to the next one
+// };
 
-const another = (req, res, next) => {
-    console.log("Another Middleware function");
-    // console.log('req.params', req.params);
-    console.log('req.url', req.url);
-    next();
-};
+// const another = (req, res, next) => {
+//     console.log("Another Middleware function");
+//     // console.log('req.params', req.params);
+//     console.log('req.url', req.url);
+//     next();
+// };
 
-const yetAnother = (req, res, next) => {
-    console.log("This will run");
-    // console.log('req.params', req.params);
-    console.log('req.url', req.url);
-    next();
-};
+// const yetAnother = (req, res, next) => {
+//     console.log("This will run");
+//     // console.log('req.params', req.params);
+//     console.log('req.url', req.url);
+//     next();
+// };
 
-app.use(logParams);
-app.use('/hello',another);
-app.use(yetAnother); // <---- this will not run if its not /hello
+// app.use(logParams);
+// app.use('/hello',another);
+// app.use(yetAnother); // <---- this will not run if its not /hello
+// app.get('/hello', //<---- if request type is /hello and is a GET type of request then and only then it will run
+// //do something 
+// )
+
+
+let v1routes = require("./routes/"); //<-- node defaults to looking for an index file in that folder when we specify folder/ in the route
+
+app.use('/api/v1', v1routes); //<-- goes to route files and comes back. versioning-set up the pattern match
+
 app.use( (req, res, next) => { // handling error before the below error handler. handling 404 
     let err = new Error('Not Found');
     err.status = 404;
@@ -40,6 +49,7 @@ app.use( (err, req, res, next) => { // for when there is an explicit error
         err: err
     });
 })
+
 
 app.listen(3000, () => {
     console.log("Listening to PORT 3000");
