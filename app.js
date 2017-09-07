6294bbae5875ea0858dc9b17fabb1b5319df5dc0
Arwa <arwa.it15@gmail.com>
2017-09-07 13:27:28 -0500

    basic express structure

---
+++ app.js
@@ -0,0 +1,46 @@
+let express = require('express');
+
+let app = express();
+
+//middleware ---> things that happen between request and response
+
+const logParams = (req, res, next) => {
+    console.log("Middleware function");
+    console.log('req.url', req.url);
+    // console.log('req', req);
+    next(); // <---- move to next middleware. done with this one. It goes to whatver comes next in app.use() but if the nexxt one does not match with the specified url, it will skip and move to the next one
+};
+
+const another = (req, res, next) => {
+    console.log("Another Middleware function");
+    // console.log('req.params', req.params);
+    console.log('req.url', req.url);
+    next();
+};
+
+const yetAnother = (req, res, next) => {
+    console.log("This will run");
+    // console.log('req.params', req.params);
+    console.log('req.url', req.url);
+    next();
+};
+
+app.use(logParams);
+app.use('/hello',another);
+app.use(yetAnother); // <---- this will not run if its not /hello
+app.use( (req, res, next) => { // handling error before the below error handler. handling 404 
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+app.use( (err, req, res, next) => { // for when there is an explicit error
+    res.status( err.status || 500); // error or generic 500-server error
+    res.json({ // we can send a json 
+        message: "Error!!",
+        err: err
+    });
+})
+
+app.listen(3000, () => {
+    console.log("Listening to PORT 3000");
+})
